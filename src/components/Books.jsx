c79fe4d2b014c39b3f573cfd90ed4b4c7e7a3d55
Nuegz07 <nugrahasaputra491@gmail.com>
2023-11-18 18:50:30 +0700

    Inisialisasi proyek

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,24 +1,23 @@
 import { Card, Heading, Image, Text, VStack } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
+
 export default function Books({ id, title, author, image, publisher, year }) {
   return (
-    <Link to={`/books/${id}`} style={{ textDecoration: 'none' }}>
-      <Card key={id} my={4} p={4} cursor='pointer' width="300px">
-        <VStack align="start">
-          <Image w={24} h={24} src={`http://localhost:3000/${image}`} alt={title} />
-          <Heading size={"md"}>{title}</Heading>
-          <Text>{author}</Text>
-          <Text>
-            <span>Publisher: </span>
-            {publisher}
-          </Text>
-          <Text>
-            <span>Year: </span>
-            {year}
-          </Text>
-        </VStack>
-      </Card>
+    <Link to={`/books/${id}`}>
+    <Card key={id} my={4} p={4} cursor='pointer'>
+      <VStack>
+        <Heading size={"md"}>
+          {title} ({year})
+        </Heading>
+        <Text>{author}</Text>
+        <Image w={24} h={24} src={`http://localhost:3000/${image}`} />
+        <Text>
+          <span>Publisher: </span>
+          {publisher}
+        </Text>
+      </VStack>
+    </Card>
     </Link>
   );
 }
